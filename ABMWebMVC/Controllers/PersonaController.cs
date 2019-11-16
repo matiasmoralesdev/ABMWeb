358b52a8a55ecdbb3aa228f4ad479f94f6e01740
Matthew M <43040428+matt12stones@users.noreply.github.com>
2019-11-15 21:48:15 -0300

    Adding things

---
+++ ABMWebMVC/Controllers/PersonaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABMWebMVC.Models;
+using ABMWebMVC.Models.ViewModels;
+
+namespace ABMWebMVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+
+            // Forma en la que nos enseñaron en el curso
+
+            // dbwebEntities db = new dbwebEntities();
+            // var listaPersonas = db.Persona.ToList();
+
+            // Forma de Tutorial
+
+            dbwebEntities db = new dbwebEntities();
+
+            var listaPersonas = (from persona in db.Persona
+                                 select new PersonaLViewModel
+                                 {
+                                     DNI = persona.DNI,
+                                     Nombre = persona.Nombre,
+                                     Apellido = persona.Apellido,
+                                     Mail = persona.Mail,
+                                     FechaNacimiento = persona.FechaNacimiento
+
+                                 }).ToList();
+
+
+            return View(listaPersonas);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ABMWebMVC/Models/ViewModels/PersonaLViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ABMWebMVC.Models.ViewModels
+{
+    public class PersonaLViewModel
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Mail { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
